4a3a4ad8ac338f40eb1c2d64fad1a7b139e8603b
franciscocavallaro <ccavallaro00@gmail.com>
2024-05-09 09:28:59 -0300

    Token verification modified. If not logged in, response is unauthorized

--- src/token/Token.ts
+++ src/token/Token.ts
@@ -1,5 +1,6 @@
 import * as jwt from "jsonwebtoken";
 import {User} from "@prisma/client";
+import {NextFunction, Request, Response} from "express";
 
 export class Token {
 
@@ -8,20 +9,27 @@ export class Token {
         if (!secret) {
             throw new Error("JWT_SECRET is not defined in the environment variables");
         }
-        return jwt.sign(data, secret, { expiresIn: "1h" });
+        return jwt.sign(data, secret, {expiresIn: "1h"});
     }
 
 
-    public static verifyToken(token: string): boolean {
-        const secret = process.env.JWT_SECRET;
-        if (!secret) {
-            throw new Error("JWT_SECRET is not defined in the environment variables");
-        }
-        try {
-            jwt.verify(token, secret);
-            return true;
-        } catch (error) {
-            return false;
+    public static verifyToken() {
+        return (req: Request, res: Response, next: NextFunction): void => {
+            const secret = process.env.JWT_SECRET;
+            if (!secret) {
+                throw new Error("JWT_SECRET is not defined in the environment variables");
+            }
+            try {
+                const token = req.headers.authorization?.split(" ")[1];
+                if (!token) {
+                    res.status(401).json({error: "Unauthorized"});
+                    return;
+                }
+                jwt.verify(token, secret);
+                next();
+            } catch (error) {
+                res.status(401).json({error: "Unauthorized"});
+            }
         }
     }
 }
